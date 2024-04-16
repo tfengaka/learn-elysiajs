f541ef1b6ad327ec5f32dd4804b98ca02a142f02
tfengaka <nthoa2.lhp.nh@gmail.com>
2024-04-17 00:00:09 +0700

    add config auth

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -10,40 +10,24 @@ export const auth = (app: Elysia) =>
 				"/signup",
 				async ({ body, set }) => {
 					const { email, name, password, username } = body;
-					// validate duplicate email address
 					const emailExists = await prisma.user.findUnique({
 						where: { email },
 						select: { id: true },
 					});
 					if (emailExists) {
 						set.status = 400;
-						return {
-							success: false,
-							data: null,
-							message: "Email address already in use.",
-						};
+						return { message: "Email address already in use." };
 					}
-
-					// validate duplicate username
 					const usernameExists = await prisma.user.findUnique({
-						where: {
-							username,
-						},
-						select: {
-							id: true,
-						},
+						where: { username },
+						select: { id: true },
 					});
 
 					if (usernameExists) {
 						set.status = 400;
-						return {
-							success: false,
-							data: null,
-							message: "Someone already taken this username.",
-						};
+						return { message: "Someone already taken this username." };
 					}
 
-					// handle password
 					const { hash, salt } = await hashPassword(password);
 					const emailHash = md5hash(email);
 					const profileImage = `https://www.gravatar.com/avatar/${emailHash}?d=identicon`;
@@ -52,11 +36,7 @@ export const auth = (app: Elysia) =>
 						data: { name, email, hash, salt, username, profileImage },
 					});
 
-					return {
-						success: true,
-						message: "Account created",
-						user: newUser,
-					};
+					return { message: "Account created", user: newUser };
 				},
 				{
 					body: t.Object({
@@ -74,26 +54,20 @@ export const auth = (app: Elysia) =>
 					const { username, password } = body;
 					// verify email/username
 					const user = await prisma.user.findFirst({
-						where: {
-							OR: [{ email: username }, { username }],
-						},
+						where: { OR: [{ email: username }, { username }] },
 						select: { id: true, hash: true, salt: true },
 					});
 
 					if (!user) {
 						set.status = 400;
-						return {
-							message: "Invalid credentials",
-						};
+						return { message: "Invalid credentials" };
 					}
 
 					// verify password
 					const match = await comparePassword(password, user.salt, user.hash);
 					if (!match) {
 						set.status = 400;
-						return {
-							message: "Invalid credentials",
-						};
+						return { message: "Invalid credentials" };
 					}
 
 					const accessToken = await jwt.sign({ userId: user.id });
@@ -115,6 +89,5 @@ export const auth = (app: Elysia) =>
 				}
 			)
 			.use(isAuthenticated)
-			// protected route
 			.get("/me", ({ user }) => user)
 	);
